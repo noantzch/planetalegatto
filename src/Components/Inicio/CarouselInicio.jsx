32a4dfeccdb38bd26bcaf96415b7951b13e22312
noel antezana <noantzch@gmail.com>
2023-05-27 00:11:34 -0300

    planetas astronautas cards

--- src/Components/Inicio/CarouselInicio.jsx
+++ src/Components/Inicio/CarouselInicio.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import "./InicioStyles.scss";
 import Carousel from 'react-bootstrap/Carousel';
 import slide1 from "../../Images/slide.jpg";
-import slide2 from "../../Images/slide2.jpg";
+import slide4 from "../../Images/slide2.jpg";
 import slide3 from "../../Images/slide3.jpg";
+import slide2 from "../../Images/slide4.jpg";
 const CarouselInicio = () => {
   return (
     <Carousel>
@@ -19,6 +20,11 @@ const CarouselInicio = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={slide3} alt="Third slide" />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slide4} alt="Foruth slide" />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
--- src/Components/QuienesSomos/Directora.jsx
+++ src/Components/QuienesSomos/Directora.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import directora from "../../Images/estimulacion1.jpg"
 import { Button } from 'react-bootstrap';
+import abi from "../../Images/abi.jpg";
 const Directora = () => {
   return (
     <div className='directoraContainer'>
         <div className='textoDirectora'>
-            <h3>Abigail Posadas</h3>
+            <h3>Emilce Posadas</h3>
             <h6>Directora General de Legatto</h6>
             <h4>"Alguna Frase Motivadora...."</h4>
         </div>
         <div className='imgDirectoraContainer'>
-            <img alt='directora' src={directora}></img> 
+            <img alt='directora' src={abi}></img> 
             <Button variant='primary' className='btn-primario'>Ver Currículum</Button>       
         </div>
     </div>
