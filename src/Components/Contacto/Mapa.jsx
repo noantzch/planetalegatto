a857148eac9ee38a9e1d6d5213b1d73bac3c433d
Luis Noel Antezana Chinchilla <109741521+noantzch@users.noreply.github.com>
2024-04-11 14:10:50 -0300

    Update Mapa.jsx

--- src/Components/Contacto/Mapa.jsx
+++ src/Components/Contacto/Mapa.jsx
@@ -1,11 +1,11 @@
 import GoogleMapReact from "google-map-react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 function Mapa() {
-  const location = { lat: -24.78515, lng: -65.41862012055392 };
+  const location = { lat: -24.7907199, lng: -65.4163422 };
   const AnyReactComponent = () => <FaMapMarkerAlt color="orange" size={32} />;
   return (
     <div className="mapaContainer">     
-        <h4>Encuentranos en Gral. Güemes 1182</h4>  
+        <h4>Encuentranos en Rivadavia 534</h4>  
         <div style={{ height: "30em", width: "50em", padding: "2em 4em" }}>
         
         <GoogleMapReact
